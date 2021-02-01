65df1bf061aab7c3fabdb6b5a308a3468f345779
Raquel Santos <raquel.maria.s.moura@accenture.com>
2021-02-01 14:18:48 -0300

    add navigation

---
+++ app/src/main/java/com/picpay/desafio/android/ui/HomeFragment.kt
@@ -0,0 +1,89 @@
+package com.picpay.desafio.android.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.picpay.desafio.android.PicPayService
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.User
+import com.picpay.desafio.android.UserListAdapter
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class HomeFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var adapter: UserListAdapter
+
+    private val url = "http://careers.picpay.com/tests/mobdev/"
+
+    private val gson: Gson by lazy { GsonBuilder().create() }
+
+    private val okHttp: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    private val service: PicPayService by lazy {
+        retrofit.create(PicPayService::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        progressBar = view.findViewById(R.id.user_list_progress_bar)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = UserListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        progressBar.visibility = View.VISIBLE
+        service.getUsers()
+            .enqueue(object : Callback<List<User>> {
+                override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                    val message = getString(R.string.error)
+
+                    progressBar.visibility = View.GONE
+                    recyclerView.visibility = View.GONE
+
+                    Toast.makeText(context, message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                    progressBar.visibility = View.GONE
+
+                    adapter.users = response.body()!!
+                }
+            })
+    }
+
+}
